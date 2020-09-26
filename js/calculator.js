16ea3ee2ab0658bffcff5e0ee07ce1391604b2c3
Redpopugay <71947954+Redpopugay@users.noreply.github.com>
2020-09-27 02:50:50 +0500

    Add files via upload

---
+++ js/calculator.js
@@ -0,0 +1,85 @@
+// Общий результат
+function resultOfCalculation() {
+	var weight = document.getElementById('weight').value; 
+	var volume = document.getElementById('volume').value;
+
+	var delivery_method = document.getElementById('deliverymethod').value;
+	var delivery_price;
+
+	var parcel_type = document.getElementById('type_parcel').value;
+	var parcel_type_price;
+
+	var total_checkout;
+
+	switch(delivery_method) {
+		case "Обычный (178 руб.)":
+			delivery_price = 178;
+			break;
+		case "Ускоренный (153 руб.)":
+			delivery_price = 153;
+			break;
+		case "ESM (323 руб.)":
+			delivery_price = 323;
+			break;
+		default:
+			console.log("ОШИБКА - Выбран не верный формат типа заказа.");
+			break;
+	}
+
+	switch(parcel_type) {
+		case "Бандероль":
+			parcel_type_price = 100;
+			break;
+		case "Письмо":
+			parcel_type_price = 150;
+			break;
+		case "Посылка":
+			parcel_type_price = 250;
+			break;
+		case "Груз":
+			parcel_type_price = 400;
+			break;
+		default:
+			console.log("ОШИБКА - Выбран не верный формат типа посылки.")
+			break;
+	}
+
+	// Проверка 
+	if (volume > 0 && weight > 0) {
+		total_checkout = (weight/5) + (volume/100) + delivery_price + parcel_type_price;
+		document.getElementById('totalcheckout').value = total_checkout;
+	}
+	else {
+		document.getElementById('totalcheckout').value = "Неверно введены данные";
+	}
+
+	console.log(total_checkout);
+
+}
+
+// Рассчет объема, вызывается из кнопки "Рассчитать", с помощью атрибута onclick
+function valueCalculate() {
+	var dimensions_lenght = document.getElementById('length_parcel').value; 
+	var dimensions_width = document.getElementById('width_parcel').value; 
+	var dimensions_height = document.getElementById('height_parcel').value;
+
+	// Объявление переменной куба и его свойств
+	let cube = {			
+		length: dimensions_lenght,
+		width: dimensions_width,
+		height: dimensions_height
+	};
+
+	// Рассчет с использованием свойств куба, с помощью функции cubeFormula
+	var cube_formula_result = cubeFormula(cube.length, cube.width, cube.height);
+	// var result = (cube_formula_result / 0.5) + (weight / 0.1);
+
+	// Вывод результата в поле "Объем"
+	document.getElementById('volume').value = cube_formula_result;
+}
+
+// Формула куба(записал в одтельную функцию для удобства)
+function cubeFormula(length, width, height) {
+	result = length * (width * height);
+	return result;
+}
